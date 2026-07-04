16ac141a78a04945f23342f49b91c76dd1a0c27d
Bharanidharan <bharanidharankpi@gmail.com>
2026-07-04 10:14:49 +0530

    Added RunChild function in sandbox.go

--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -1,17 +1,34 @@
 package sandbox
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
+	"runtime"
 	"syscall"
 )
+
+const newRoot string = "/tmp/rootfs"
+const oldRoot string = "put_old"
+
+func must(err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		log.Fatalf("Error at %s:%d - %v", file, line, err)
+	}
+}
 
 func RunCode(code []byte) error{
 	return createSanbox(code)
 }
 
 func createSanbox(code []byte) error {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
 	//Synchronization pipe to ensure that parent is the one that spawned the child process
 	readFd, writeFd, err := os.Pipe()
 
@@ -34,9 +51,8 @@ func createSanbox(code []byte) error {
 
 	cmd := exec.Command("/proc/self/exe", "child")
 	cmd.ExtraFiles = []*os.File{readFd, readCode}
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
 	hostUid := os.Getuid()
 	hostGid := os.Getgid()
@@ -63,5 +79,68 @@ func createSanbox(code []byte) error {
 
 	writeFd.Write([]byte{0})
 
+	if err := cmd.Wait(); err != nil {
+		errLine := stderr.String()
+		fmt.Println(errLine)
+		return errors.New("Error: Command exited with status code 1")
+	}
+
+	result := stdout.String()
+	fmt.Println(result)
+
 	return nil
+}
+
+func RunChild() {
+
+	syncPipe := os.NewFile(3, "sync")
+	codePipe := os.NewFile(4, "code")
+
+	if syncPipe == nil || codePipe == nil {
+		os.Exit(1)
+	}
+
+	code, err := io.ReadAll(codePipe)
+	must(err)
+	codePipe.Close()
+
+	buf := make([]byte, 1)
+	syncPipe.Read(buf)
+	syncPipe.Close() 
+
+	must(syscall.Sethostname([]byte("container")))
+
+	// Pivot root cannot be done on shared mount propagation so make it private recursive and then create a bind mount on the new root directory
+	must(syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""))
+	must(syscall.Mount(newRoot, newRoot, "", syscall.MS_BIND|syscall.MS_REC, ""))
+
+
+	procDir := newRoot + "/proc"
+	// Everybody can read it though only the owner can do crud
+	must(os.MkdirAll(procDir, 0755))
+	flags := uintptr(syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV)
+	must(syscall.Mount("proc", procDir, "proc", flags, ""))
+
+	pivotDir := newRoot + "/" + oldRoot
+	// 0700 - Cause we want only the owner to read, update and write it and no other person should be able to access it
+	must(os.MkdirAll(pivotDir, 0700))
+
+	must(syscall.PivotRoot(newRoot, pivotDir))
+
+	must(syscall.Chdir("/"))
+
+	// Unmount the older filesystem
+   must(syscall.Unmount("/"+oldRoot, syscall.MNT_DETACH))
+
+	// Delete that unmounted old root
+	must(os.Remove("/" + oldRoot))
+
+	must(os.WriteFile("/tmp/submission.py", code, 0644))
+
+	//Remove all the admin capabilities before running the command
+	for i := 0; i < 64; i++ {
+		unix.Prctl(unix.PR_CAPBSET_DROP, uintptr(i), 0, 0, 0)
+	}
+
+	must(syscall.Exec("/usr/bin/python3", []string{"python3", "/tmp/submission.py"}, os.Environ()))
 }
